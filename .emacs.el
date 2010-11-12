a8f8817dca421385b8a835182f4820e7b6fe3eb2
snj14 <esnj14@gmail.com>
2010-11-13 01:53:32 +0900

    migemoいれた

--- .emacs.el
+++ .emacs.el
@@ -1130,10 +1130,16 @@
 ;;; ------------------------------------------------------------------
 
 ;;; migemo.el
-(when (and linux-p
-           (locate-library "migemo"))
-   ;; sudo apt-get install migemo
-  (load "migemo"))
+;;; linux : http://d.hatena.ne.jp/ser1zw/20100825/1282663086
+(when linux-p
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs"))
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  (migemo-init))
 (when darwin-p
     (setq migemo-command "/usr/local/bin/cmigemo")
     (setq migemo-options '("-q" "--emacs"))
