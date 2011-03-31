3f8d50eeb7f9a9f633806dd1e53a68b8636c72a1
snj14 <esnj14@gmail.com>
2011-04-01 00:19:46 +0900

    e2wm.el周りの設定変更

--- .emacs.el
+++ .emacs.el
@@ -783,24 +783,41 @@
   (add-to-list 'popwin:special-display-config '("*Warnings*" :noselect t))
   )
 
-;;; e2wm
-(lazyload (e2wm:start-management) "e2wm" "e2wm")
-(setq e2wm:c-code-recipe
-  '(| (:left-size-ratio 0.2)
-      (- (:upper-size-ratio 0.7)
-         files history)
-      (- (:upper-size-ratio 0.7)
-         (| (:right-size-ratio 0.2)
-            main imenu)
-         sub)))
+;;; e2wm.el
+(lazyload (e2wm:start-management) "e2wm" "e2wm"
+  (setq e2wm:c-code-recipe
+        '(| (:left-size-ratio 0.2)
+            (- (:upper-size-ratio 0.7)
+               files history)
+            (- (:upper-size-ratio 0.7)
+               (| (:right-size-ratio 0.2)
+                  main imenu)
+               sub)))
+  ;; ファイラ：C-hで上の階層へ
+  (e2wm:add-keymap
+   e2wm:def-plugin-files-mode-map
+   '(("C-h" . e2wm:def-plugin-files-updir-command))
+   e2wm:prefix-key)
+
+  (e2wm:add-keymap
+   e2wm:pst-minor-mode-keymap
+   '(("<M-left>" . e2wm:dp-code) ; codeへ変更
+     ("<M-right>" . e2wm:dp-two) ; twoへ変更
+     ("<M-up>" . e2wm:dp-doc) ; docへ変更
+     ("<M-down>" . e2wm:dp-dashboard) ; dashboardへ変更
+     ("C-," . e2wm:pst-history-forward-command) ; 履歴を進む
+     ("C-." . e2wm:pst-history-back-command) ; 履歴をもどる
+     ("prefix L" . ielm)
+     ("M-m" . e2wm:pst-window-select-main-command)
+     ) e2wm:prefix-key)
+)
+
+
 (global-set-key (kbd "M-+") 'e2wm:start-management)
 
 ;; windmove.el
 ;; shift + <cursor>
 (windmove-default-keybindings)
-(setq windmove-wrap-around t)
-
-
 
 ;;; ------------------------------------------------------------------
 ;;; Edit
