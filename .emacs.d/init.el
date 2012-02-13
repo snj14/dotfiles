c83e07ab3f3c5cf8607460c571e68557c06e7714
snj14 <esnj14@gmail.com>
2012-02-14 00:08:07 +0900

    diredの改善

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -569,6 +569,17 @@
                 ("lisp" "el" "pl" "c" "h" "cc" "js" font-lock-constant-face)))
              ))
 
+;; open marked files
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map (kbd "F") 'my-dired-find-marked-files)
+     (defun my-dired-find-marked-files (&optional arg)
+       "Open each of the marked files, or the file under the point, or when prefix arg, the next N files "
+       (interactive "P")
+       (let* ((fn-list (dired-get-marked-files nil arg)))
+         (mapc 'find-file fn-list)))))
+
+
 ;;; ------------------------------------------------------------------
 ;;; Major Mode
 ;;; ------------------------------------------------------------------
